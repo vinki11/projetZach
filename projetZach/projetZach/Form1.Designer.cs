14f34507cf5a8cfbbc18184ddf7b90caf3313e27
Vincent <gelinas.vincent@gmail.com>
2017-01-23 21:33:46 -0500

    Commit du projet

---
+++ projetZach/projetZach/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace projetZach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_generer = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnParcourir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_generer
+            // 
+            this.btn_generer.Location = new System.Drawing.Point(329, 149);
+            this.btn_generer.Name = "btn_generer";
+            this.btn_generer.Size = new System.Drawing.Size(75, 23);
+            this.btn_generer.TabIndex = 0;
+            this.btn_generer.Text = "Generer";
+            this.btn_generer.UseVisualStyleBackColor = true;
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(12, 24);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(311, 20);
+            this.txtFileName.TabIndex = 2;
+            // 
+            // btnParcourir
+            // 
+            this.btnParcourir.Location = new System.Drawing.Point(329, 24);
+            this.btnParcourir.Name = "btnParcourir";
+            this.btnParcourir.Size = new System.Drawing.Size(75, 23);
+            this.btnParcourir.TabIndex = 3;
+            this.btnParcourir.Text = "Parcourir";
+            this.btnParcourir.UseVisualStyleBackColor = true;
+            this.btnParcourir.Click += new System.EventHandler(this.btn_parcourir_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 194);
+            this.Controls.Add(this.btnParcourir);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btn_generer);
+            this.Name = "Form1";
+            this.Text = "Generateur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_generer;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnParcourir;
+    }
+}
+
---
+++ projetZach/projetZach/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+using System.Windows.Forms;
+
+
+namespace projetZach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_parcourir_click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Fichier csv(*.csv)|*.csv|Tous les fichiers (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = false;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            FileStream fs = myStream as FileStream;
+                            string fileName = "";
+                            fileName = fs.Name;
+                            txtFileName.Text = Path.GetFileName(fileName);
+                            Convert_CSV_To_Excel(fileName);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur : Impossible de lire le fichier - Zach tu fais surment quelquechose de pas correct /n" + ex.Message);
+                }
+            }
+        }
+    
+
+        private void Convert_CSV_To_Excel(string filename)
+        {
+            /*string csvFileName = @"FL_insurance_sample.csv";
+            string excelFileName = @"FL_insurance_sample.xls";*/
+
+            string csvFileName = filename;
+            string excelFileName = Path.GetFileNameWithoutExtension(filename) + ".xlsx";
+
+            string worksheetsName = Path.GetFileNameWithoutExtension(filename);
+
+            bool firstRowIsHeader = false;
+
+            var format = new ExcelTextFormat();
+            format.Delimiter = ',';
+            format.EOL = "\r";              // DEFAULT IS "\r\n";
+                                            // format.TextQualifier = '"';
+            String outputPath = "";
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                outputPath = fbd.SelectedPath;
+            }
+
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFileName)))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(worksheetsName);
+                worksheet.Cells["A1"].LoadFromText(new FileInfo(csvFileName), format, OfficeOpenXml.Table.TableStyles.Medium27, firstRowIsHeader);
+                package.SaveAs(new FileInfo(outputPath + "/" + excelFileName));
+            }
+
+            /*Console.WriteLine("Finished!");
+            Console.ReadLine();*/
+        }
+
+    }
+}

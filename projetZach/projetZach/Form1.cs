9f6b1ada4a93192d5753c527abb78cb9e5f1da79
vinki11 <gelinas.vincent@gmail.com>
2017-02-23 15:04:18 -0500

    Refactore un peu, travail sur les checkbox pour drawer le chart a chaque clique de checkbox Decouverte dun esti de bug paske ya des colonnes qui sont pas juste en chiffre

--- projetZach/projetZach/Form1.cs
+++ projetZach/projetZach/Form1.cs
@@ -15,8 +15,9 @@ namespace projetZach
 {
     public partial class Form1 : Form
     {
+        int test = 0;
         string fileName = "";
-        DataTable originalDt;
+        //DataTable originalDt;
         DataTable dt;
         int maxCycle = 0;
         CultureInfo ci;
@@ -34,83 +35,7 @@ namespace projetZach
             ci = new CultureInfo("fr-CA");
         }
 
-        private void btn_parcourir_file_click(object sender, EventArgs e)
-        {
-            Stream myStream = null;
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-
-            openFileDialog1.InitialDirectory = "c:\\";//mettre directory par defaut de Zach ?
-            openFileDialog1.Filter = "Fichier csv(*.csv)|*.csv|Tous les fichiers (*.*)|*.*";
-            openFileDialog1.FilterIndex = 1;
-            openFileDialog1.RestoreDirectory = false;
-
-            List<String> csvLines = new List<String>();
-
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                try
-                {
-                    if ((myStream = openFileDialog1.OpenFile()) != null)
-                    {
-                        using (myStream)
-                        {
-                            FileStream fs = myStream as FileStream;
-                            fileName = fs.Name;
-                            txtFileName.Text = Path.GetFileName(fileName);
-                            dt = new DataTable();
-                            originalDt = new DataTable();
-
-                            String csvLine;
-                            StreamReader reader = new StreamReader(fileName);
-                            while ((csvLine = reader.ReadLine()) != null)
-                            {
-                                csvLines.Add(csvLine.Replace('.',',')); //change en virgule mais ca fix pas le problème d'affichage a Zach
-                            }
-                            //On construit le datatable avec le csv
-                            int lineCount = 0;
-                            foreach (String readerLine in csvLines)
-                            {
-                                var values = readerLine.Split('\t');
-                                if (lineCount != 0)
-                                {
-                                    dt.Rows.Add();
-                                }
-                                int colCount = 0;
-                                foreach (String value in values)
-                                {
-                                    //On ajoute les colonnes au DataTable
-                                   if (lineCount == 0)
-                                    {
-                                      dt.Columns.Add(colName(value,dt)); //Fonction recursive qui ajoute des espace a coté du nom de colonne pour évité les 12 millions de doublons. Datatable permet pas de doublons de nom de colonnes
-                                      if (colCount == 77 || colCount == 78 || colCount == 79)
-                                        {
-                                            dt.Columns[colCount].DataType = typeof(Double);
-                                        }
-                                    }
-                                   else
-                                   {
-                                        decimal dec;
-                                        if (Decimal.TryParse(value, out dec))
-                                            dt.Rows[lineCount - 1][colCount] = decimal.Parse(value.ToString(), ci ).ToString("G29");
-                                        else
-                                            dt.Rows[lineCount - 1][colCount] = value;
-                                            
-                                   }
-                                    colCount++;
-                                }
-                                lineCount++;
-                            }
-                        }
-                        originalDt = dt.Copy();
-                        DrawChart(dt);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Erreur : Impossible de lire le fichier - Zach tu fais surment quelquechose de pas correct \n" + ex.Message);
-                }
-            }
-        }
+        #region Private Function
 
         private void DrawChart(DataTable data)
         {
@@ -122,11 +47,6 @@ namespace projetZach
             double[] y = new double[dt.Rows.Count];
             for (int i = 0; i < dt.Rows.Count - 1; i++)
             {
-                //Calcul le cycle maximum pour afficher les checkbox
-                if (Convert.ToInt32(dt.Rows[i][0]) > maxCycle)
-                {
-                    maxCycle = Convert.ToInt32(dt.Rows[i][0]);
-                }
                 //Definit les données pour le graphique
                 x[i] = dt.Rows[i][0].ToString(); /*decimal.Parse(dt.Rows[i][0].ToString()).ToString("G29")*/;
                 y[i] = Convert.ToDouble(dt.Rows[i][78]);
@@ -136,12 +56,6 @@ namespace projetZach
             graphZach.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineWidth = 0;
             graphZach.ChartAreas["ChartArea1"].AxisX.LabelStyle.Enabled = false;
 
-            //Affichage des checkbox
-            for (var i = 1; i <= maxCycle; i++)
-            {
-                this.Controls.Find("checkbox" + i, true)[0].Enabled = true;
-                ((CheckBox)this.Controls.Find("checkbox" + i, true)[0]).Checked = true;
-            }
         }
 
         private DataTable PutZeros(DataTable data, string[] noCycles)
@@ -197,10 +111,147 @@ namespace projetZach
                 MessageBox.Show("L'exportation est terminé");
             }
             
-
-            
         }
-        
+
+        private void EnableCheckBox()
+        {
+            //Affichage des checkbox
+            for (var i = 1; i <= maxCycle; i++)
+            {
+                this.Controls.Find("checkbox" + i, true)[0].Enabled = true;
+                ((CheckBox)this.Controls.Find("checkbox" + i, true)[0]).Checked = true;
+                ((CheckBox)this.Controls.Find("checkbox" + i, true)[0]).CheckedChanged += CheckChanged;
+            }
+        }
+
+        private void DisableCheckBox()
+        {
+            //Affichage des checkbox
+            for (var i = 1; i <= 13; i++)
+            {
+                ((CheckBox)this.Controls.Find("checkbox" + i, true)[0]).CheckedChanged -= CheckChanged;
+                ((CheckBox)this.Controls.Find("checkbox" + i, true)[0]).Checked = false;
+                this.Controls.Find("checkbox" + i, true)[0].Enabled = false;
+            }
+        }
+
+        private string colName(string value, DataTable data)
+        {
+            string colonneName = "";
+
+            if (data.Columns.Contains(value))
+                colonneName = colName(value + " ", data);
+            else
+                colonneName = value;
+
+            return colonneName;
+        }
+
+        #endregion
+
+        #region Button Function
+
+        private void btn_parcourir_file_click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";//mettre directory par defaut de Zach ?
+            openFileDialog1.Filter = "Fichier csv(*.csv)|*.csv|Tous les fichiers (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = false;
+
+            List<String> csvLines = new List<String>();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            FileStream fs = myStream as FileStream;
+                            fileName = fs.Name;
+                            txtFileName.Text = Path.GetFileName(fileName);
+                            dt = new DataTable();
+                            //originalDt = new DataTable();
+
+                            String csvLine;
+                            StreamReader reader = new StreamReader(fileName);
+                            while ((csvLine = reader.ReadLine()) != null)
+                            {
+                                csvLines.Add(csvLine.Replace('.', ',')); //change en virgule mais ca fix pas le problème d'affichage a Zach
+                            }
+                            //On construit le datatable avec le csv
+                            int lineCount = 0;
+                            foreach (String readerLine in csvLines)
+                            {
+                                var values = readerLine.Split('\t');
+                                if (lineCount != 0)
+                                {
+                                    dt.Rows.Add();
+                                }
+                                int colCount = 0;
+                                foreach (String value in values)
+                                {
+                                    //On ajoute les colonnes au DataTable
+                                    if (lineCount == 0)
+                                    {
+                                        dt.Columns.Add(colName(value, dt)); //Fonction recursive qui ajoute des espace a coté du nom de colonne pour évité les 12 millions de doublons. Datatable permet pas de doublons de nom de colonnes
+                                        if (colCount == 77 || colCount == 78 || colCount == 79)
+                                        {
+                                            dt.Columns[colCount].DataType = typeof(Double);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        decimal dec;
+                                        if (Decimal.TryParse(value, out dec))
+                                            dt.Rows[lineCount - 1][colCount] = decimal.Parse(value.ToString(), ci).ToString("G29");
+                                        else
+                                            dt.Rows[lineCount - 1][colCount] = value;
+
+                                    }
+                                    colCount++;
+                                }
+                                lineCount++;
+                            }
+                        }
+                        maxCycle = 0;
+                        for (int i = 0; i < dt.Rows.Count - 1; i++)
+                        {
+                            //Calcul le cycle maximum pour afficher les checkbox
+                            if (Convert.ToInt32(dt.Rows[i][0]) > maxCycle)
+                            {
+                                maxCycle = Convert.ToInt32(dt.Rows[i][0]);
+                            }
+                        }
+                        //originalDt = dt.Copy();
+                        DisableCheckBox();
+                        DrawChart(dt);
+                        EnableCheckBox();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur : Impossible de lire le fichier - Zach tu fais surment quelquechose de pas correct \n" + ex.Message);
+                }
+            }
+        }
+
+        private void btnParcourirPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = System.Environment.SpecialFolder.MyComputer;
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                outputPath = fbd.SelectedPath;
+                txtOutputPath.Text = outputPath;
+            }
+
+        }
 
         private void btn_generer_Click(object sender, EventArgs e)
         {
@@ -224,13 +275,13 @@ namespace projetZach
                 //Si il y a des erreurs on affiche un message, sinon on fait l'exportation
                 if (errorMessage != "")
                 {
-                    MessageBox.Show("Impossible de faire la génération \n" + errorMessage); 
+                    MessageBox.Show("Impossible de faire la génération \n" + errorMessage);
                 }
                 else
                 {
                     ExportToExcel(dt, fileName, outputPath);
                 }
- 
+
 
             }
             catch
@@ -238,18 +289,6 @@ namespace projetZach
             {
                 MessageBox.Show("Erreur : Une erreur est survenue lors de l'exportation - Envoit un texte à Vince avec un screenshot pour voir \n" + ex.Message);
             }
-        }
-
-        private string colName(string value, DataTable data)
-        {
-            string colonneName = "";
-
-            if (data.Columns.Contains(value))
-                colonneName = colName(value + " ", data);
-            else
-                colonneName = value;
-
-            return colonneName;
         }
 
         private void CheckChanged(object sender, EventArgs e)
@@ -269,21 +308,10 @@ namespace projetZach
 
             dt = PutZeros(dt, cycleZeroArray);
 
-            DrawChart(dt);
+            //DrawChart(dt);
         }
 
-        private void btnParcourirPath_Click(object sender, EventArgs e)
-        {
-            FolderBrowserDialog fbd = new FolderBrowserDialog();
-            fbd.RootFolder = System.Environment.SpecialFolder.MyComputer;
-            
-            if (fbd.ShowDialog() == DialogResult.OK)
-            {
-                outputPath = fbd.SelectedPath;
-                txtOutputPath.Text = outputPath;
-            }
-
-        }
+        #endregion
     }
 
 }
